d33d05286b5ef735037dfde708774f76cd769160
Shuta <shuta.shibue@gmail.com>
2022-07-15 09:46:16 +0900

    save

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { Message, Client, DMChannel } from 'discord.js'
 import dotenv from 'dotenv'
 import {Fetch} from './fetchSheet'
-import * as fs from 'fs'
 import * as sqlite3 from 'sqlite3';
 const characters = [
     '一姫', '二階堂美樹', '軽庫娘', '藤田佳奈', '三上千織', '相原舞', '撫子', '八木唯',
@@ -17,8 +16,9 @@ const characters = [
 dotenv.config()
 
 // database init
-const db = new sqlite3.Database('db.sqlite3');
-    const tableMake = sql_table()
+const db = new sqlite3.Database('db.sqlite3')
+const tableMake = sql_table()
+db.run(tableMake);
 
 // discord client init
 const client = new Client({
@@ -36,15 +36,20 @@ client.once('ready', () => {
 
 client.on('messageCreate', async (message: Message) => {
     if (message.author.bot) return
+    if (message.content.startsWith('Man')){
+        const t = message.author.tag
+        db.run(`update pd set earned = 100  where tag = '${t}'`)
+    }
     if (message.channel.type !== "DM") return
     if (!client.user) return
     const botID = client.user.id
     const userID = message.author.id
+    const userTag = message.author.tag    
     if (message.mentions.users.has(botID)){
         const ch = client.users.cache.get(userID)?.dmChannel
 
         ch?.send(
-            '何をしますか?\n1: 割り振り状況、pt残高を確認したい\n2: 投票、修正がしたい\n3:新たにプレイヤー登録しました。確認してください。'
+            '何をしますか?\n1: 割り振り状況、pt残高を確認したい\n2: 投票したい\n3:新たにプレイヤー登録しました。確認してください。'
             )
         const filter = (msg:Message) => msg.author.id === message.author.id
         ch?.awaitMessages({ filter, max: 1, time: 10 * 1000 })
@@ -53,11 +58,11 @@ client.on('messageCreate', async (message: Message) => {
             const selector =  collected.first()?.content            
             switch (selector) {
                 case "1":
-                    await checkStatus(ch)
+                    await sendVotingStatus(ch, userTag)
                     break;
 
                 case "2":
-                    await vote(ch)
+                    await vote(ch, userTag)
                     break;
 
                 case "3":
@@ -76,34 +81,32 @@ client.on('messageCreate', async (message: Message) => {
 
 client.login(process.env.TOKEN)
 
-async function checkStatus(channel:DMChannel) {
-    console.log("hi");
-    
-    channel.send("投票状況/個人")
-}
-async function vote(channel:DMChannel) {
+async function vote(ch:DMChannel, tag:string) {
 
-    channel.send(voting_Msg())
+    const balance = await sendVotingStatus(ch, tag)
+    ch.send('投票したいキャラクターの番号を入力してください。')
     const filter = (msg:Message) => msg.author.id !== client.user?.id
 
     // select character
-    channel.awaitMessages({ filter, max: 1, time: 20 * 1000 })
+    ch.awaitMessages({ filter, max: 1, time: 20 * 1000 })
     .then(async collected => {
-        if (!collected.size) return channel.send('タイムアウトしました')
+        if (!collected.size) return ch.send('タイムアウトしました。投票をキャンセルします')
         const charID =  Number(collected.first()?.content)
-        const tag = collected.first()?.author.tag
         const votedChar = characters[charID]
 
-        channel.send('何票入れますか?  pt残高:')//残高を計算する処理を追加する
-        channel.awaitMessages({ filter, max: 1, time: 20 * 1000 })
+        ch.send(`${votedChar}に投票します。何票入れますか?\n半角で入力してください。(例:10)\n自然数以外を入力すると投票がキャンセルされます。`)
+        ch.awaitMessages({ filter, max: 1, time: 30 * 1000 })
             .then(async collected => {
-                if (!collected.size) return channel.send('タイムアウトしました')
-                let amount =  Number(collected.first()?.content)
+                if (!collected.size) return ch.send('タイムアウトしました。投票をキャンセルします')
+                let desiredVoteAmt =  Number(collected.first()?.content)
                 db.serialize(() => {
-                    db.get(`select ${votedChar} from playerdata where tag = '${tag}'`, (err:any, row:any) => {                        
-                        amount += Number(row[votedChar]);
-                        //マイナスの処理を追加しておく
-                        db.run(`update playerData set ${votedChar} = ${amount}  where tag = '${tag}'`)
+                    db.get(`select ${votedChar} from pd where tag = '${tag}'`, (err:any, row:any) => { 
+                        const votedRecord = Number(row[votedChar])                        
+                        if (isNaN(desiredVoteAmt) || desiredVoteAmt < 1 ) return ch.send('無効な値です。投票をキャンセルします')
+                        if(balance < desiredVoteAmt) return ch.send('残高不足です。投票をキャンセルします。')
+                        desiredVoteAmt += Math.floor(votedRecord);
+                        db.run(`update pd set ${votedChar} = ${desiredVoteAmt}  where tag = '${tag}'`)
+                        ch.send(`${votedChar}に${desiredVoteAmt}票入れました。投票を終了します。`)
                     })
                 })
             })
@@ -112,20 +115,22 @@ async function vote(channel:DMChannel) {
 async function updatePlayerinDB(){
     const pData = await Fetch()    
     db.serialize(() => {
-        db.run(tableMake);
         for (let i = 0; i < pData.length; i++) {
             const tag = pData[i][0]
             const name = pData[i][1]
-            db.run(`INSERT or ignore INTO playerData(tag, name) VALUES (?, ?)`, [tag, name]);
+            db.run(`INSERT or ignore INTO pd(tag, name) VALUES (?, ?)`, [tag, name]);
         }
     })
 }
 
+
 function sql_table(){
-    let order = `CREATE TABLE IF NOT EXISTS playerData(
+    let order = `CREATE TABLE IF NOT EXISTS pd(
         tag text primary key,
         name text,
-        earned id not null default 0 `
+        earned int not null default 0,
+        adjust int not null default 0
+        `
     
     for (let i = 0; i < characters.length; i++) {
         order += ', ' + characters[i] + ' int not null default 0'
@@ -134,11 +139,29 @@ function sql_table(){
 
     return order
 }
-function voting_Msg(){
-    let msg = `投票したいキャラクターの番号を指定してください。\n`
-    for (let i = 0; i < characters.length; i++) {
-        const id = i<10? i+' ' : i+''
-        msg += `ID: ${id}      ${characters[i]}\n`   
-    }
-    return msg
+async function sendVotingStatus(ch:DMChannel, tag:string){
+    const balance:number = await new Promise((resolve)=>{
+        let msg = ''
+        db.get(`select * from pd where tag = '${tag}'`, (err:any, row:any) => { 
+            if (err) return ch.send('プレイヤー登録をしてください。最近した場合は、「3:新たにプレイヤー登録しました。確認してください。」を選択してください。\n終了します。')
+            let ptUsed = 0                       
+            for (let i = 0; i < characters.length; i++) {
+                const voted = Number(row[characters[i]])
+                const id = i<10? i+' ' : i+''
+                msg += `ID: ${id}    ${voted}票    ${characters[i]}\n`
+                ptUsed += voted
+            }
+            const balance = Number(row['earned']) - ptUsed
+            msg += `\nあなたのポイント残高: ${balance}\n`
+            ch.send(msg)
+            resolve(balance)
+        })
+    })
+    return balance
+}
+
+function isAdmin(tag:string){
+    const admins = ['えびてん#8658', 'poko#3397', 'konaso#1033', 'ぴにぷ#7148']
+    if (admins.includes(tag)) return true
+    else return false
 }
